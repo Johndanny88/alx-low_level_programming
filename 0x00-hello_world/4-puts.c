c2b6209f2123e5316adc7f961097937c9ba095d1
johndanny88 <johndannyjw2020@gmail.com.com>
2022-09-12 01:05:56 -0700

    programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  *
- *  * main - Entry point
+ *  *main - Entry point
  *
  *   *
  *
- *    * Return: Always 0 (Success)
+ *    *Return: Always 0(success)
  *
  *     */
 
@@ -14,8 +14,8 @@ int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 
-		return (0);
+			return (0);
 
 }
