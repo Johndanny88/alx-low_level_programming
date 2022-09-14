0f63a1d5129b3e371a1089a551f98b94fdb6cccf
johndanny88 <johndannyjw2020@gmail.com.com>
2022-09-14 08:02:23 -0700

    alphebets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.c"
+
+/**
+ * prints _alphabet - program
+ *
+ * returns 0
+ */
+void print_alphabet(void)
+{
+	int x;
+	
+	for (x = 97; x <= 122; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n')
+}
