47ed4a8aa7fa1b183e0257bd4f5becb0b970de84
johndanny88 <johndannyjw2020@gmail.com.com>
2022-09-14 00:05:15 -0700

    putchar

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/* program to print out _putchar */
+
+int main(void)
+{
+	char text[9] = "_putchar";
+
+		int x;
+
+
+	for (x = 1; x < 9; x++);
+
+             _putchar(text[9]);
+            _putchar('\n);
+    
+         return (0);
+	 
+}	 
