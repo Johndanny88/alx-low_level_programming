caeb6ff688f7a177417860be09fa91b649d4af62
johndanny88 <johndannyjw2020@gmail.com.com>
2022-09-15 12:47:18 -0700

    put character

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,7 @@
-#include stdio.h
-
-int _putchar(char c)
-void print_alphabet(void);
+ 1 #ifndef MAIN_H
+ 2 #define MAIN_H
+ 3
+ 4 int _putchar(char c);
+ 5 void printMul(void)                                                                                       
+ 6                                                                                                          
+ 7 #enif /* MAIN_H *
