14cb8d1afe56b5672ca3a74438d18bdf6b645d06
johndanny88 <johndannyjw2020@gmail.com.com>
2022-09-19 11:27:19 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,5 @@
- 1 #ifndef MAIN_H
- 2 #define MAIN_H
- 3
- 4 int _putchar(char c);
- 5                                                                                        
- 6                                                                                                          
- 7 #enif /* MAIN_H *
+#ifndef MAIN_H
+#define MAIN_H
+
+ int _putchar(char c);                                                                                                          
+#enif /* MAIN_H */
