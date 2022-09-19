826f297c1923e8fbd99c54134dee89201c323156
johndanny88 <johndannyjw2020@gmail.com.com>
2022-09-19 15:10:52 -0700

    pionters

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *function that takes a pointer to an int as parameter and updates the value it points to to 98
+ *
+ * return always (0)
+ */
+
+void reset_to_98(int *n)
+{
+	int n= 98;
+	int *p = &n;
+
+	_putchar(n);
+	{
+		_putchar('\n');
+	}
+
+	return (0)
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - call other function
+ *
+ * return (0)
+ *
+ */
+int main(void)
+{
+	printMul();
+
+	return (0)
+}
