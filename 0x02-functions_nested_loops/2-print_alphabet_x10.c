116783bf4a74d1134c3f66dafe4ddb0eb3e24b63
johndanny88 <johndannyjw2020@gmail.com.com>
2022-09-19 13:06:49 -0700

    abc x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *
+ * print_alphabet_x10 - Prints a-z in lowercase 10 times
+ *
+ * return : nothing (null void)
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+
+	while (x < 10)
+
+	{
+		char alphabet = "a";
+
+		while (alphabet <= "z")
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+		
+		_putchar('\n');
+		x++;
+	}
+}
